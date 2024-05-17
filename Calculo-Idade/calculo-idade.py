73748fda42f3d3e2ea6a8fcd524eab1462e830b5
Tiago <tiagoalbertoo17@gmail.com>
2024-05-17 18:35:58 -0300

    Criando interface com PySimpleGUI

--- Calculo-Idade/calculo-idade.py
+++ Calculo-Idade/calculo-idade.py
@@ -1,37 +1,116 @@
+import PySimpleGUI as sg
 
-diaNasc = input("Qual o dia do seu nascimento?")
-mesNasc = input("Qual o mês do seu nascimento?")
-anoNasc = input("Qual o ano do seu nascimento?")
+sg.theme('Reddit')
+'''
+layout = [
+    [sg.Text('usuario'), sg.InputText(key='usuario')],
+    [sg.Text('senha'), sg.InputText(key='senha')],
+    [sg.Button('Calcular')],
+    [sg.Text('',key='idade')],
+] 
 
-diaAtual = input("Qual o dia atual?")
-mesAtual = input("Qual o mês atual?")
-anoAtual = input("Qual o ano atual?")
+janela = sg.Window('Tela de login',layout)
 
+while True:
+    eventos, valores = janela.read()
 
-def CalculaIdade(diaNasc,mesNasc,anoNasc,diaAtual,mesAtual,anoAtual):
+    if eventos == sg.WINDOW_CLOSED:
+       break
+    if eventos == 'Calcular':
+        janela['idade'].update('15 anos')
+
+        print('Foi') 
+
+janela.close()
+
+'''
+
+coluna1 = [
+    [sg.Text("Dia Nascimento", font="arial 12")],
+    [sg.Text("Mês Nascimento", font="arial 12")],
+    [sg.Text("Ano Nascimento", font="arial 12")],
+    [sg.Text("Dia Atual", font="arial 12")],
+    [sg.Text("Mês Atual", font="arial 12")],
+    [sg.Text("Ano Atual", font="arial 12")],
+    [sg.Button('Calcular')],
     
-    difAno = anoAtual - anoNasc
-    
-    if mesAtual < mesNasc | (mesAtual == mesNasc & diaAtual < diaNasc):
-        difAno -= 1
 
-    difMes = mesAtual - mesNasc
+]
 
-    if difMes < 0:
-        difMes += 12
+coluna2 = [
+
+    [sg.Input(font="arial 12", key="DiaNasc", size=(30, 20))],
+    [sg.Input(font="arial 12", key="MesNasc", size=(30, 20))],
+    [sg.Input(font="arial 12", key="AnoNasc", size=(30, 20))],
+    [sg.Input(font="arial 12", key="DiaAtual", size=(30, 20))],
+    [sg.Input(font="arial 12", key="MesAtual", size=(30, 20))],
+    [sg.Input(font="arial 12", key="AnoAtual", size=(30, 20))],
+    [sg.Text('',key='idade')],
+]
+
+layout = [
+    [sg.Text("Calculadora de Idade", font="arial 20 bold")],
+    [sg.Column(coluna1), sg.Column(coluna2)],
+
+]
+
+janela = sg.Window(
+    "Análise",
+    element_justification="left",
+    element_padding=(0, 10),
+    layout=layout,
+    size=(500, 400),
+    finalize=True,
+)
+
+while True:
+
+    eventos, valores = janela.read()
+
+    diaNasc = int(valores['DiaNasc'])
+    mesNasc = int(valores['MesNasc'])
+    anoNasc = int(valores['AnoNasc'])
+
+    diaAtual = int(valores['DiaAtual'])
+    mesAtual = int(valores['MesAtual'])
+    anoAtual = int(valores['AnoAtual'])
 
 
-    difDia = diaAtual - diaNasc
+    #função para calcular idade
+    def CalculaIdade(diaNasc,mesNasc,anoNasc,diaAtual,mesAtual,anoAtual):
+        
+        difAno = anoAtual - anoNasc
+        
+        if mesAtual < mesNasc | (mesAtual == mesNasc & diaAtual < diaNasc):
+            difAno -= 1
 
-    if diaAtual < 0:
-        difDia += 30
+        difMes = mesAtual - mesNasc
 
         if difMes < 0:
             difMes += 12
-            difAno -= 1
 
 
-    return f"Você tem {difAno} anos,{difMes} meses e {difDia} dias"
+        difDia = diaAtual - diaNasc
 
-idade = CalculaIdade(diaNasc,mesNasc,anoNasc,diaAtual,mesAtual,anoAtual)
-print(idade)
+        if difDia < 0:
+            difDia += 30
+
+            if difMes < 0:
+                difMes += 12
+                difAno -= 1
+
+
+        return f"Você tem {difAno} anos, {difMes} meses e {difDia} dias"
+
+    idade = CalculaIdade(diaNasc,mesNasc,anoNasc,diaAtual,mesAtual,anoAtual)
+
+
+
+    if eventos == sg.WINDOW_CLOSED:
+       break
+    if eventos == 'Calcular':
+        janela['idade'].update(idade)
+        
+
+
+janela.close()
