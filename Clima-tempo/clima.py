a308f0a12d19ea415a82755fd660df23a18837cc
Tiago <tiagoalbertoo17@gmail.com>
2024-05-31 10:25:59 -0300

    adicionando django

--- Clima-tempo/clima.py
+++ Clima-tempo/clima.py
@@ -1,12 +1,19 @@
 import requests
 
 
+chave_api = 'ab1db5d5894858d8593b78c175f66e2a'
+
+cidade = 'São Paulo'
+
+url = f"http://api.openweathermap.org/data/2.5/forecast?q={cidade}&units=metric&appid={chave_api}&lang=pt_br"
 
 
-url = "http://apiadvisor.climatempo.com.br/api/v1/anl/synoptic/locale/BR?token=286d19584ffa29dff7425640b92463eb"
+response = requests.get(url).json()
+
+print(response['city']['name'] )
+print(response['list'][0]['main']['temp'])
+print(response['list'][0]['weather'][0]['description'])
+print(response['list'][0]['main']['humidity'])
+print(response['list'][0]['wind']['speed'])
 
 
-
-response = requests.get(url)
-
-print(response.json())
