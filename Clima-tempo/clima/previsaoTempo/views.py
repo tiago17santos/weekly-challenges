b1f41ed112ab406ca582a133b6b61ca9f3d17e8c
Tiago <tiagoalbertoo17@gmail.com>
2024-05-31 11:56:38 -0300

    inserindo templates django

--- Clima-tempo/clima/previsaoTempo/views.py
+++ Clima-tempo/clima/previsaoTempo/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+import requests
 
 # Create your views here.
 def previsao(request):
-    return HttpResponse("Hello world")
+    template = loader.get_template('app.html')
+   
+    cidade = request.GET.get('search')
+
+    chave_api = 'ab1db5d5894858d8593b78c175f66e2a'
+
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={cidade}&units=metric&appid={chave_api}&lang=pt_br"
+
+
+    response = requests.get(url).json()
+
+
+    return render(request, 'app.html', response)
