18e1b6b1425a755adad717899df2d098bd33701f
Tiago <tiagoalbertoo17@gmail.com>
2024-05-31 14:19:46 -0300

    finalizando compra de upgrades

--- Cliques-automaticos/cookie-clicker.py
+++ Cliques-automaticos/cookie-clicker.py
@@ -14,7 +14,7 @@ class CookieClicker():
                     "xpath": '/html/body/div/div[2]/div[15]/div[8]/button'
                 },
                 "upgrades":{
-                    "xpath":'/html/body/div/div[2]/div[19]/div[3]/div[6]/div[$$NUMBER$$]/div[3]'
+                    "xpath":'/html/body/div/div[2]/div[19]/div[3]/div[6]/div[$$NUMBER$$]'
                 },
                 "language":{
                     "xpath": '/html/body/div/div[2]/div[12]/div/div[1]/div[1]/div[10]'
@@ -32,14 +32,35 @@ class CookieClicker():
         time.sleep(5)
 
     def seleciona_idioma(self):
-        click_mouse = self.driver.find_element(By.XPATH,'/html/body/div/div[2]/div[12]/div/div[1]/div[1]/div[10]')
+        click_mouse = self.driver.find_element(By.XPATH, self.site_map['buttons']['language']['xpath'])
         ActionChains(self.driver).click(click_mouse).perform()
         time.sleep(5)
     
 
     def clicar_cookie(self):
-        click_mouse = self.driver.find_element(By.XPATH,'/html/body/div/div[2]/div[15]/div[8]/button')
+        click_mouse = self.driver.find_element(By.XPATH, self.site_map['buttons']['cookie']['xpath'])
 
+        ActionChains(self.driver).click(click_mouse).perform()
+
+    def pegar_melhor_upgrade(self):
+        encontrei = False
+        elemento_atual = 2
+
+        while not encontrei:
+            objeto = self.site_map['buttons']['upgrades']['xpath'].replace('$$NUMBER$$', str(elemento_atual))
+            classes_objeto = self.driver.find_element(By.XPATH, objeto).get_attribute('class')
+            
+            if not 'enable' in classes_objeto:
+                encontrei = True
+            else:
+                elemento_atual += 1
+        return elemento_atual - 1
+
+    def comprar_upgrade(self):
+        print(self.pegar_melhor_upgrade())
+        
+        objeto = self.site_map['buttons']['upgrades']['xpath'].replace('$$NUMBER$$', str(self.pegar_melhor_upgrade()))
+        click_mouse = self.driver.find_element(By.XPATH, objeto)
         ActionChains(self.driver).click(click_mouse).perform()
 
 
@@ -50,8 +71,10 @@ biscoito.seleciona_idioma()
 i = 0
 
 while True:
-    if i % 500 == 0 and i != 0:
-        time.sleep(10)
+    if i % 250 == 0 and i != 0:
+        time.sleep(5)
+        biscoito.comprar_upgrade()
+        time.sleep(5)
     biscoito.clicar_cookie()
 
     i += 1    
